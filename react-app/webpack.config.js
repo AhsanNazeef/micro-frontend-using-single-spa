3073ab9054a2c24eb6afc92e92fee8bc20cca14c
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-11-27 10:49:24 +0500

    configured react app in single spa

---
+++ react-app/webpack.config.js
@@ -0,0 +1,37 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "demo",
+    projectName: "react-app",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'postcss-loader',
+          ],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.css'],
+    },
+  });
+};
+
